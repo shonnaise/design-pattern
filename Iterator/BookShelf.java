1112c19ee234e977d3ac7b579fccde5a4a5e4ae6
shonnaise <shonnaise@users.noreply.github.com>
2019-06-04 23:35:41 +0900

    Array to ArrayList

--- Iterator/BookShelf.java
+++ Iterator/BookShelf.java
@@ -1,18 +1,19 @@
+import java.util.*;
+
 public class BookShelf implements Aggregate {
-    private Book[] books;
+    private ArrayList<Book> books;
     private int last = 0;
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf() {
+        this.books = new ArrayList<>();
     }
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        this.books.add(book);
     }
     public int getLength() {
-        return last;
+        return books.size();
     }
     public Iterator iterator() {
         return new BookShelfIterator(this);
--- Iterator/Main.java
+++ Iterator/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        BookShelf bookShelf = new BookShelf(4);
+        BookShelf bookShelf = new BookShelf();
         bookShelf.appendBook(new Book("Around the World in 80 Days"));
         bookShelf.appendBook(new Book("Bible"));
         bookShelf.appendBook(new Book("Cinderella"));
         bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+        bookShelf.appendBook(new Book("Java Design Pattern"));
         Iterator it = bookShelf.iterator();
         while (it.hasNext()) {
             Book book = (Book)it.next();
